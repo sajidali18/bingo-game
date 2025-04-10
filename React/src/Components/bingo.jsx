f58e6ab747cffb893f5c36a9323a1428a19e5b70
sajidali <mohammedsajid@gmail.com>
2025-04-10 15:07:37 +0530

    changes

--- React/src/Components/bingo.jsx
+++ React/src/Components/bingo.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://bingo-game-1.onrender.com');
 
 const BingoGame = () => {
     const [username, setUsername] = useState('');
